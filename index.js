4bb043c9e9715176de58768a2e3c8ccf24e20ec2
Dulce Jaramillo <djaramillo@LABA.PSWAS.LOCAL>
2023-08-21 09:03:48 -0700

    practice functions

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// const addstrings = (sentence) => {
+
+//     let arrayOfLetters = []
+//     arrayOfLetters = sentence.split('')
+//     arrayOfLetters.forEach((letters, index) => {
+//         if (letters === ' ') {
+//             arrayOfLetters.splice(index, 1)
+//         }
+//     })
+//     return arrayOfLetters.length
+// }
+
+// console.log(addstrings("how many letters are in this sentence?"));
+
+const addstrings = () =>{
+    
+}
